7b1b7b519a21b53bf1fffae0b2c85b7101943ef9
spadanbalikpapan <spadanbalikpapan@gmail.com>
2026-07-06 02:09:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = "rudal-spadan-v1";
+const CACHE_NAME = "rudal-spadan-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
